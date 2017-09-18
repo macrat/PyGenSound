d12c69edbc7ed6b691849994443de67435ed8ee2
MacRat <m@crat.jp>
2017-09-18 23:45:57 +0900

    Fixed travis configure

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ tests = lambda : doctest.DocTestSuite('gensound')
 setup(name='PyGenSound',
       version='0.0.1',
       description='A sound generating and computing library',
-      long_description=open('README.md').read(),
+      long_description=open('README.md', encoding='utf-8').read(),
       author='MacRat',
       author_email='m@crat.jp',
       url='http://github.com/macrat/gensound',
