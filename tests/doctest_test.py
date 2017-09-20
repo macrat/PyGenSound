ff38e872092c7ce105cd3f85b4b9ae85c7ad48f6
MacRat <m@crat.jp>
2017-09-20 17:08:27 +0900

    Added pep8 and doctest for tests

--- tests/doctest_test.py
+++ tests/doctest_test.py
@@ -2,9 +2,14 @@ import doctest
 import unittest
 
 import gensound
+import tests
 
 
 class DocTest(unittest.TestCase):
-    def test_doctest(self):
+    def test_doctest_gensound(self):
         failure, total = doctest.testmod(gensound)
+        self.assertEqual(failure, 0)
+
+    def test_doctest_tests(self):
+        failure, total = doctest.testmod(tests)
         self.assertEqual(failure, 0)
--- tests/pep8_test.py
+++ tests/pep8_test.py
@@ -1,15 +1,22 @@
+import typing
 import unittest
 
 import pep8
 
 
 class PEP8Test(unittest.TestCase):
-    def test_pep8(self):
+    def run_pep8_test(self, modules: typing.Iterable[str]):
         pep8.DEFAULT_IGNORE = 'W503'
         checker = pep8.StyleGuide()
 
-        result = checker.check_files(['gensound'])
+        result = checker.check_files(modules)
 
         message = 'pep8: {} errors / {} warnings'.format(result.get_count('E'),
                                                          result.get_count('W'))
         self.assertEqual(result.total_errors, 0, message)
+
+    def test_pep8_gensound(self):
+        self.run_pep8_test(['gensound'])
+
+    def test_pep8_tests(self):
+        self.run_pep8_test(['tests'])
